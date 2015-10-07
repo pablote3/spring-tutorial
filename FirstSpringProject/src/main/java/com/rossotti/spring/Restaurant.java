2a3998d237085c3ac6a4883e1ba340528073e1a1
Paul Rossotti <rossotti.paul@gmail.com>
2015-10-07 15:07:37 -0700

    completed tutorial

--- FirstSpringProject/src/main/java/com/rossotti/spring/Restaurant.java
+++ FirstSpringProject/src/main/java/com/rossotti/spring/Restaurant.java
@@ -1,9 +1,52 @@
 package com.rossotti.spring;
 
+import java.util.List;
+import javax.annotation.*;
+
 public class Restaurant {
 
+	private String welcomeNote;
+	private IHotDrink hotDrink;
+	private List<String> restaurantWaitersList;
+	
+	//constructor injection
+//	Restaurant(IHotDrink hotDrink) {
+//		this.hotDrink = hotDrink;
+//	}
+	
+	//setter injection
+//	public void setHotDrink(IHotDrink hotDrink) {
+//		this.hotDrink = hotDrink;
+//	}
+	
+	public void setRestaurantWaitersList(List<String> restaurantWaitersList) {
+		this.restaurantWaitersList = restaurantWaitersList;
+	}
+
+	public void displayWaitersNames() {
+		System.out.println("All waiters working in Restaurant : " + restaurantWaitersList);
+	}
+	
+	public void prepareHotDrink() {
+		hotDrink.prepareHotDrink();
+	}
+
+	public void setWelcomeNote(String welcomeNote) {
+		this.welcomeNote = welcomeNote;
+	}
+	
 	public void greetCustomer() {
-		System.out.println("Welcome to our Restaurant!");
+		System.out.println(welcomeNote);
+	}
+	
+//	@PostConstruct
+	public void init() {
+		System.out.println("Restaurant Bean is going through init");
+	}
+	
+//	@PreDestroy
+	public void destroy() {
+		System.out.println("Restaurant Bean is going to destroy");
 	}
 	
 }
--- FirstSpringProject/src/main/java/com/rossotti/spring/TestSpringProject.java
+++ FirstSpringProject/src/main/java/com/rossotti/spring/TestSpringProject.java
@@ -1,6 +1,7 @@
 package com.rossotti.spring;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpringProject {
@@ -8,8 +9,18 @@ public class TestSpringProject {
 		//interface for accessing Spring framework
 		ApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
 		
+		((AbstractApplicationContext)context).registerShutdownHook();
+		
 		//request object from application context
 		Restaurant restaurantObj = (Restaurant)context.getBean("restaurantBean");
 		restaurantObj.greetCustomer();
+		//restaurantObj.prepareHotDrink();
+		//restaurantObj.displayWaitersNames();
+		
+//		Restaurant restaurantObj1 = (Restaurant)context.getBean("restaurantBean");
+//		restaurantObj1.setWelcomeNote("Object1 setting welcome note property");
+//		restaurantObj1.greetCustomer();		
+//		Restaurant restaurantObj2 = (Restaurant)context.getBean("restaurantBean");
+//		restaurantObj2.greetCustomer();
 	}
 }
