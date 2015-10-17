f30fb2e089dc671377cf1d4f5f20b231cc1ff324
Paul Rossotti <rossotti.paul@gmail.com>
2015-10-17 15:55:54 -0700

    introduce restrictions

--- javaBrains-Hibernate/src/com/rossotti/dto/UserDetails.java
+++ javaBrains-Hibernate/src/com/rossotti/dto/UserDetails.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 
 @Entity
+@NamedQuery(name="UserDetails.byId", query="from UserDetails where userId = ?")
 public class UserDetails {
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	private int userId;
--- javaBrains-Hibernate/src/com/rossotti/hibernate/HibernateTest.java
+++ javaBrains-Hibernate/src/com/rossotti/hibernate/HibernateTest.java
@@ -2,10 +2,14 @@ package com.rossotti.hibernate;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 
 import com.rossotti.dto.UserDetails;
 
@@ -18,11 +22,22 @@ public class HibernateTest {
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		
-		Query query = session.createQuery("from UserDetails");
-		query.setFirstResult(1);
-		query.setMaxResults(2);
-		List<UserDetails> users = (List<UserDetails>)query.list();
+		String minUserId = "1";
+//		Query query = session.createQuery("from UserDetails where userId > ?");
+//		query.setInteger(0, Integer.parseInt(minUserId));
+		
+//		Query query = session.getNamedQuery("UserDetails.byId");
+//		query.setInteger(0, 1);
+//		List<UserDetails> users = (List<UserDetails>)query.list();
+		
+//		Criteria criteria = session.createCriteria(UserDetails.class);
+//		criteria.add(Restrictions.eq("userName", "User2"));	
 
+		Criteria criteria = session.createCriteria(UserDetails.class)
+									.setProjection(Projections.property("userId"))
+									.addOrder(Order.asc("userId"));
+		
+		List<UserDetails> users = (List<UserDetails>)criteria.list();
 		
 		session.getTransaction().commit();
 		session.close();
