132fd6a021e4b7cb53b19416668b0cb714cc26e8
Paul Rossotti <rossotti.paul@gmail.com>
2019-12-17 18:02:08 -0800

    initial commit

---
+++ cloudNativeJava-DemoService/src/main/java/com/example/cloudNativeJavaDemoService/CloudNativeJavaDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudNativeJavaDemoService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeJavaDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeJavaDemoServiceApplication.class, args);
+	}
+
+}
---
+++ cloudNativeJava-DemoService/src/test/java/com/example/cloudNativeJavaDemoService/CloudNativeJavaDemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudNativeJavaDemoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeJavaDemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
